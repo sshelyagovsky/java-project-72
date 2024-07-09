415bddf45b5767e78e225ee0fb97cfe923e7368e
s.shelyagovsky <s.shelyagovsky@gmail.com>
2024-07-09 23:49:33 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
+[![Java CI](https://github.com/sshelyagovsky/java-project-72/actions/workflows/main.yml/badge.svg)](https://github.com/sshelyagovsky/java-project-72/actions/workflows/main.yml)
 [![Actions Status](https://github.com/sshelyagovsky/java-project-72/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/sshelyagovsky/java-project-72/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/9471d2cfdb1b97a1613b/maintainability)](https://codeclimate.com/github/sshelyagovsky/java-project-72/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/9471d2cfdb1b97a1613b/test_coverage)](https://codeclimate.com/github/sshelyagovsky/java-project-72/test_coverage)
---
+++ app/build.gradle.kts
@@ -0,0 +1,45 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    id("java")
+    id("checkstyle")
+    id("io.freefair.lombok") version "8.6"
+    id("com.github.ben-manes.versions") version "0.51.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.h2database:h2:2.2.224")
+    implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
+    implementation("org.apache.commons:commons-text:1.11.0")
+    implementation("gg.jte:jte:3.1.9")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("io.javalin:javalin-bundle:6.1.3")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+
+    testImplementation("org.assertj:assertj-core:3.25.3")
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
+}
