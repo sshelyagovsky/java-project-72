93537e7c4304696aa43270ebd5ebd52def403e37
s.shelyagovsky <s.shelyagovsky@gmail.com>
2024-07-12 20:02:42 +0300

    add org.postgresql:postgresql:42.7.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
     implementation("io.javalin:javalin:6.1.3")
     implementation("io.javalin:javalin-bundle:6.1.3")
     implementation("io.javalin:javalin-rendering:6.1.3")
-
+    implementation("org.postgresql:postgresql:42.7.2")
     testImplementation("org.assertj:assertj-core:3.25.3")
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
