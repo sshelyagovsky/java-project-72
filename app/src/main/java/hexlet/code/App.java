b8b5ec9b4445baa068d712a8d6727c5c5e2b4af0
s.shelyagovsky <s.shelyagovsky@gmail.com>
2024-07-13 22:11:05 +0300

    fix

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -26,7 +26,7 @@ public class App {
     }
 
     private static String getDatabaseUrl() {
-        return System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project");
+        return System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:postgresql://dpg-cq9butdds78s739c5o80-a:5432/project4_kus7?password=u7ao5512M6C5NsuJir14upQ6ItQOX9UI&user=project4_kus7_user");
     }
 
     private static String readResourceFile(String fileName) throws IOException {
