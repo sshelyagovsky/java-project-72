d1f6dd69cd69b7c3c822e96c4818ef64f06a168e
s.shelyagovsky <s.shelyagovsky@gmail.com>
2024-07-17 23:37:33 +0300

    step 6

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,5 +1,7 @@
 package hexlet.code;
 
+import hexlet.code.model.Url;
+import hexlet.code.repository.UrlRepository;
 import io.javalin.Javalin;
 import io.javalin.testtools.JavalinTest;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,4 +28,41 @@ public class AppTest {
             assertThat(response.code()).isEqualTo(200);
         });
     }
+
+    @Test
+    public void testUrlsPage() {
+        JavalinTest.test(app, (server, client) ->{
+            var response = client.get("/urls");
+            assertThat(response.code()).isEqualTo(200);
+        });
+    }
+
+    @Test
+    public void testCreateUrl() {
+        JavalinTest.test(app, (server, client) -> {
+            var requestBody = "name=urlname";
+            var response = client.post("/urls", requestBody);
+            assertThat(response.code()).isEqualTo(200);
+            assertThat(response.body().string().contains("urlname"));
+        });
+    }
+
+    @Test
+    public void testUrlPage() throws SQLException {
+        Url url = new Url("urlname");
+        UrlRepository.save(url);
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/urls/" + url.getId());
+            assertThat(response.code()).isEqualTo(200);
+        });
+    }
+
+    @Test
+    void testUrlNotFound() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/urls/999999");
+            assertThat(response.code()).isEqualTo(404);
+        });
+    }
+
 }
